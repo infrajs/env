202808a29e6e49ca5c7a7d419da432858acd446a
Anton Kiyatkin <a.kiyatkin@itlf.ru>
2020-05-12 22:58:14 +0400

    fix

--- infra.js
+++ infra.js
@@ -1,7 +1,5 @@
-import { Env } from "/vendor/infrajs/env/Env.js"
-import { Template } from '/vendor/infrajs/template/Template.js'
+import { Controller } from '/vendor/infrajs/controller/src/Controller.js'
 
-Template.scope['Env'] = {};
-Template.scope['Env']['get'] = function (name) { return Env.get(name) };
-Template.scope['Env']['name'] = function () { return Env.name(); };
-Template.scope['Env']['is'] = function () { return Env.is(); };
+Controller.hand('init', async () => {
+	await import('./init.js')
+})
