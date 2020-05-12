51cc37df2c9c61de5207ecabd9053785ccb713f8
Anton Kiyatkin <a.kiyatkin@itlf.ru>
2020-05-12 20:28:58 +0400

    fix

--- infra.js
+++ infra.js
@@ -5,13 +5,3 @@ Template.scope['Env'] = {};
 Template.scope['Env']['get'] = function (name) { return Env.get(name) };
 Template.scope['Env']['name'] = function () { return Env.name(); };
 Template.scope['Env']['is'] = function () { return Env.is(); };
-
-/*Controller.parsedAdd(function(layer){
-	//Можно получить список вызываемых в шаблоне функций и точней определить зависит слой или нет от окружения
-	//Но есть ещё другие расширения Region Lang
-
-
-	if (!layer.data&&!layer.json) return '';
-	//Если установили значение вручную такое же как определено автоматически.
-	return Env.name()+(Env.is()?'1':'0');
-});*/
